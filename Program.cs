fc5faf236e51f91cb84f243f0dcebbf2befcb62a
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-06 14:12:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_forLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lets make a traingle
+
+            Console.Write("Enter a num:");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < num; i++) 
+            {
+                for (int j = 0; j <=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
